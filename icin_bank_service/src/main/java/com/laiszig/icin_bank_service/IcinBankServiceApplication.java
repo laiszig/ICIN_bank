95e45148990c07148910d7292622bf9094d78c3d
lais <laisziegler@gmail.com>
2023-08-08 17:33:13 -0300

    initial commit

---
+++ icin_bank_service/src/main/java/com/laiszig/icin_bank_service/IcinBankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.icin_bank_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcinBankServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcinBankServiceApplication.class, args);
+    }
+
+}
---
+++ icin_bank_service/src/test/java/com/laiszig/icin_bank_service/IcinBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.icin_bank_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcinBankServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
