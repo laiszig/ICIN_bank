39e5b3141027c4509d3e4f9ac237ac47c28ea549
lais <laisziegler@gmail.com>
2023-10-25 15:32:20 -0300

    user home screen

--- icin_bank_frontend/src/app/app-routing.module.ts
+++ icin_bank_frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountCreationComponent } from './account/account-creation/account-creation.component';
 import { AccountListComponent } from './account/account-list/account-list.component';
 import { AdminScreenComponent } from './account/home/admin-screen/admin-screen.component';
+import { UserScreenComponent } from './account/home/user-screen/user-screen.component';
 
 const routes: Routes = [
   {"path":"account/add", component: AccountCreationComponent},
   {"path":"accounts", component: AccountListComponent},
-  {"path":"admin", component: AdminScreenComponent},
+  {"path":"admin/home", component: AdminScreenComponent},
+  {"path":"user/home", component: UserScreenComponent},
 ];
 
 @NgModule({
