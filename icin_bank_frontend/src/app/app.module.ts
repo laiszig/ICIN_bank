3b261a18985a7af30c5651966c28334b71f8cc17
lais <laisziegler@gmail.com>
2023-10-25 10:45:36 -0300

    image slider

--- icin_bank_frontend/src/app/app.module.ts
+++ icin_bank_frontend/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageSliderComponent } from './image-slider/image-slider.component';
+import { NgImageSliderModule } from 'ng-image-slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSliderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgImageSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
