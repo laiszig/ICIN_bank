956227fd91f6b8a3d4907b1985ee38b2f7588b57
lais <laisziegler@gmail.com>
2023-10-26 14:29:35 -0300

    add crossOrigin annotations

--- icin_bank_service/src/main/java/com/laiszig/icin_bank_service/controller/TransactionController.java
+++ icin_bank_service/src/main/java/com/laiszig/icin_bank_service/controller/TransactionController.java
@@ -5,14 +5,12 @@ import com.laiszig.icin_bank_service.entity.Transaction;
 import com.laiszig.icin_bank_service.service.TransactionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TransactionController {
 
     private final TransactionService transactionService;
