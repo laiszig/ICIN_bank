e31b8fe4e5840ed602e1f98a667546fea17c9687
lais <laisziegler@gmail.com>
2023-10-30 14:25:02 -0300

    checkbook approval

---
+++ icin_bank_frontend/src/app/checkbook/checkbook.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Checkbook } from './checkbook';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckbookService {
+
+  private baseUrl = 'http://localhost:8080/admin/checkbook';
+
+  constructor(private http: HttpClient) { }
+
+  getAllCheckbookRequests(): Observable<Checkbook[]> {
+    return this.http.get<Checkbook[]>(`${this.baseUrl}`);
+  }
+
+  approveCheckbookStatus(id: number): Observable<Checkbook> {
+    return this.http.put<Checkbook>(`${this.baseUrl}/approve/${id}`, {});
+  }
+
+  denyCheckbookStatus(id: number): Observable<Checkbook> {
+    return this.http.put<Checkbook>(`${this.baseUrl}/deny/${id}`, {});
+  }
+}
---
+++ icin_bank_frontend/src/app/checkbook/checkbook.ts
@@ -0,0 +1,5 @@
+export interface Checkbook {
+    id: number;
+    status: string;
+  }
+  
